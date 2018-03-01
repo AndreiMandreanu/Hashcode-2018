a97dc1f99a20eb63ecfe3782f6b5791de6a34506
Karol <ks2117@ic.ac.uk>
2018-03-01 19:32:43 +0000

    working on availability

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,23 +1,39 @@
+import java.util.ArrayList;
+
 public class Simulation {
 
-  private final List<Car> cars;
+  private final List<Vehicle> vehicles;
   private final List<Ride> rides;
   private final int bonus;
   private final int numberOfSteps;
-  private int currentStep;
+  private int currentStep = 0;
   private final int width;
   private final int heigth;
 
 
 
-  public Simulation(List<Car> cars, List<Ride> rides, int width, int height, int bonus, int numberOfSteps){
-    this.cars = cars;
+  public Simulation(List<Vehicle> vehicles, List<Ride> rides,
+      int width, int height, int bonus, int numberOfSteps){
+    this.vehicles = vehicles;
     this.rides = rides;
     this.bonus = bonus;
     this.numberOfSteps = numberOfSteps;
-    this.currentStep = 0;
     this.width = width;
     this.heigth = height;
   }
+
+  public void assignRide() {
+    Ride ride = rides.get(0);
+    rides.remove(ride);
+    List<Vehicle> availableVehicles = getAvailableVehicles(ride);
+    getClosestVehicles(availableVehicles).getRides.add(ride);
+  }
+
+  public Vehicle getAvailableVehicles(Ride ride) {
+    for(Vehicle v : vehicles) {
+      ride
+    }
+  }
+
 
 }
