99434001af0460ab1d0ba4178925a3060ba31c1e
Karol <ks2117@ic.ac.uk>
2018-03-01 18:44:08 +0000

    simulation class

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,6 +1,23 @@
 public class Simulation {
 
-  public Simulation(int height, int width){
+  private final List<Car> cars;
+  private final List<Ride> rides;
+  private final int bonus;
+  private final int numberOfSteps;
+  private int currentStep;
+  private final int width;
+  private final int heigth;
+
+
+
+  public Simulation(List<Car> cars, List<Ride> rides, int width, int height, int bonus, int numberOfSteps){
+    this.cars = cars;
+    this.rides = rides;
+    this.bonus = bonus;
+    this.numberOfSteps = numberOfSteps;
+    this.currentStep = 0;
+    this.width = width;
+    this.heigth = height;
   }
 
 }
